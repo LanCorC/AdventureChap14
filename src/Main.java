026033353515cbc97a9980ad991d1d261b278578
LanCorC <lanc3.corcega@gmail.com>
2023-03-13 15:36:20 +1300

    Completing Section 14.187: Adventure Game challenge
    (How to add, and read for input, for a set known values in the context of player controls)
    
    note: all sections 12+ i complete has is not the latest 2023 re-master, still pending

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,12 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
+    //Challenge: allow full words or phrases, then move
+    //e.g. Go West, run South, East, N should all be acceptable
+
+
     private static Map<Integer, Location> locations =
             new HashMap<Integer, Location>();
-
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -22,45 +25,74 @@ public class Main {
         locations.get(1).addExit("E", 3);
         locations.get(1).addExit("S", 4);
         locations.get(1).addExit("N", 5);
-//        locations.get(1).addExit("Q", 0);
 
         locations.get(2).addExit("N", 5);
-//        locations.get(2).addExit("Q", 0);
 
         locations.get(3).addExit("W", 1);
-//        locations.get(3).addExit("Q", 0);
 
         locations.get(4).addExit("N", 1);
         locations.get(4).addExit("W", 2);
-//        locations.get(4).addExit("Q", 0);
 
         locations.get(5).addExit("S", 1);
         locations.get(5).addExit("W", 2);
-//        locations.get(5).addExit("Q", 0);
-
 
         int loc = 1;
         while (true) {
             System.out.println(locations.get(loc).getDescription());
-            if(loc == 0) {
+            if (loc == 0) {
                 break;
             }
 
             Map<String, Integer> exits = locations.get(loc).getExits();
             System.out.print("Available exits are: ");
-            for (String exit: exits.keySet()) {
+            for (String exit : exits.keySet()) {
                 System.out.print(exit + ", ");
             }
             System.out.println();
 
+//            my implementation below:
+//            Limitations: trust that the user will appropriately enter correct words, else..
+//            No, I wanna stay -> read as “N” north if exit is valid
+//            String[] direction = scanner.nextLine().
+//                    toUpperCase().split(" ");
+//            int size = direction.length;
+//            for (String input : direction){
+//                size--;
+//                input = input.substring(0, 1);
+//                if (exits.containsKey(input)) {
+//                    loc = exits.get(input);
+//                    System.out.println("Valid exit selected: " + input);
+//                } else if(size == 0) {
+//                    System.out.println("You cannot go in that direction");
+//                }
+//            }
+
+            //His implementation:
+            Map<String, String> vocabulary = new HashMap<>();
+            vocabulary.put("NORTH", "N");
+            vocabulary.put("WEST", "W");
+            vocabulary.put("SOUTH", "S");
+            vocabulary.put("EAST", "E");
+
+
             String direction = scanner.nextLine().toUpperCase();
+            if (direction.length() > 1) {
+                String[] words = direction.split(" ");
+                for (String word : words) {
+                    if (vocabulary.containsKey(word)) {
+                        direction = vocabulary.get(word);
+                    }
+                }
+            }
+
             if (exits.containsKey(direction)) {
                 loc = exits.get(direction);
             } else {
-                System.out.println("You cannot go in that direction");
+                System.out.println("Invalid direction.");
             }
         }
-
-
+        //Limitations:
+        //trusts users use strictly sole input, N, Q, S etc or
+        //trusts users with phrases strictly uses word e.g. “please go north” not “please go n”
     }
 }
